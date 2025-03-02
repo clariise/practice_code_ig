a0644c47542efc98fdfc80b171b140ddf9c84fc3
clariise <clcarmona001@gmail.com>
2025-03-03 00:04:49 +0800

    added arrays & practice js

--- Javascript-course/scripts/08-array-loops-todo-list.js
+++ Javascript-course/scripts/08-array-loops-todo-list.js
@@ -1,9 +1,30 @@
-const todoList = [];
+const todoList = [""];
 
-function addTodo(){
- const inputElement = document.querySelector('.js-name-input');
- const name = inputElement.value;
+renderTodoList();
 
- todoList.push(name);
- console.log(todoList);
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
+
+//1. button to add todo
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input"); //selecting the input element
+  const name = inputElement.value; //getting the value of the input element
+
+  todoList.push(name);
+  // console.log(todoList); //visible on our console (arrays)
+  inputElement.value = ""; //clear the input field
+
+  renderTodoList();
+}
+
+//LOOPING
