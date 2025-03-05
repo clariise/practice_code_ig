7011de48f2e01597a021472bb56f8e129a1d9480
clariise <clcarmona001@gmail.com>
2025-03-05 13:14:33 +0800

    exercises about number and math

---
+++ Javascript-course-exercises/lesson-01/1b.js
@@ -0,0 +1,32 @@
+document.body.innerHTML = "Good afternoon!";
+alert("Goodmorning!");
+console.log(10 + 8 + 20);
+console.log(10 + 5);
+console.log(20 - 5);
+console.log("Good morning!");
+console.log("Clarisse Carmona");
+
+//math
+
+console.log(10 + 3 * 8 + 5);
+
+console.log((10 + 3 * 8 + 5) / 3);
+
+console.log((18.5 + 2 * 7.5) * 0.1);
+
+console.log((18.5 + 2 * 7.5) * 0.2);
+
+console.log((4793 + 499) / 100);
+
+console.log(Math.round((4793 + 499) / 100)); //calculate using math.round //53
+
+console.log(Math.floor(2.8)); //rounds a number down
+console.log(Math.ceil(2.5)); //rounds a number up
+console.log(Math.round(2.4)); //rounds a number to the nearest integer
+
+//calculate celsius to fahrenheit = (Celcius * 9/5) + 32 while
+//fareheit to celsius = (Fahrenheit - 32) * 5/9
+console.log((25 * 9) / 5 + 32);
+console.log(((86 - 32) * 5) / 9);
+
+console.log((-5 * 9) / 5 + 32);
---
+++ Javascript-course-exercises/lesson-01/README.md
@@ -0,0 +1,5 @@
+# JAVASCRIPT BASIC - Exercises
+
+1. Use alert to display content in a pop up
+2. Using math to calculate naumbers
+3. Use dom like document.body.innerHtml= ''; to get/change/replace the HTML content
